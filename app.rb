f7e11167f56bf881acb0318b76f03cd99537a77d
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-02-27 02:53:25 +0000

    all changes

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require "sinatra"
-require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+    erb(:form)
 end
+
+post("/submit") do
+    @name = params[:name]
+    @age = params[:age]
+    @email = params[:email]
+    @learning_mode = params[:learning_mode]
+    @favourite_color = params[:favourite_color]
+    @hobby = params[:hobby]
+    @subscription = params[:subscription] == "on" ? "Yes" : "No"
+
+    erb(:result)
+
+end
+
+
